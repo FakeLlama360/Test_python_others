8d42d917efd91b45d762c7519dc7ec744349c872
Samuel Mlýnek <sammlynek7@gmail.com>
2024-05-06 20:15:53 +0200

    the space ship

--- gamesometing.py
+++ gamesometing.py
@@ -1,6 +1,10 @@
 import pygame
 from pygame.locals import *
 
+#define FPS
+clock = pygame.time.Clock()
+fps = 60
+
 
 screen_width = 600
 screen_height = 800
@@ -15,9 +19,30 @@ bg = pygame.image.load("img/bg.png")
 def draw_bg():
     screen.blit(bg, (0,0))
 
+
+#create space ship class
+
+class Spaceship(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("img/spaceship.png")
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+
+#create sprite groups
+spaceship_group = pygame.sprite.Group()
+
+
+#create player 
+spaceship = Spaceship(int(screen_width / 2), screen_height - 100)
+spaceship_group.add(spaceship)
+
+
+
 run = True
 while run:
 
+    clock.tick(fps)
 
     #draw backroud
     draw_bg()
@@ -27,6 +52,9 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    #update sprite groups
+    spaceship_group.draw(screen)
+    
 
     pygame.display.update()
 
