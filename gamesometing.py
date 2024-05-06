a8dc5129b6ce6163fc33c19ce9c4b9a0986ca05d
Samuel Mlýnek <sammlynek7@gmail.com>
2024-05-06 20:03:58 +0200

    it can RUN {{{

--- gamesometing.py
+++ gamesometing.py
@@ -10,14 +10,24 @@ pygame.display.set_caption('Space Invaders')
 
 
 #load image
-bg = pygame.image.load("")
+bg = pygame.image.load("img/bg.png")
+
+def draw_bg():
+    screen.blit(bg, (0,0))
 
 run = True
 while run:
+
+
+    #draw backroud
+    draw_bg()
 
     #event handlers
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+
+    pygame.display.update()
 
 pygame.quit()
